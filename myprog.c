3f0332f1307f3dc433dd3da1b94a5e331ebc7323
Dhanushka sandakelum <62172201+DhanushkaSandakelum@users.noreply.github.com>
2023-11-27 15:05:51 +0530

    HOTD 2 added

---
+++ myprog.c
@@ -0,0 +1,10 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(void)
+{
+    printf(1, "Our first program on xv6 %d | add_space_size: %d\n", getYear(), getmysize());
+
+    exit();
+}
--- sysproc.c
+++ sysproc.c
@@ -89,3 +89,16 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+
+int
+sys_getYear(void)
+{
+  return 1975;
+}
+
+int
+sys_getmysize(void)
+{
+  return myproc()->sz;
+}
